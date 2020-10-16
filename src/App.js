f403c6bb846321fb7164e05d694efe7369afb70c
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2020-10-16 07:00:24 +0300

    Day 3 (SectorsList component, added blank jsx layout)

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import SectorsList from "./components/SectorsList/SectorsList";
 import gsLogo from './assets/GC_logo.svg';
 import itnLogo from './assets/ITN_logo.svg';
 import shidLogo from './assets/shid_logo_white.svg';
+import useScreenSize from "./utils/useScreenSize";
 // import heroBg from './assets/hero-bg.jpg';
 
 function App() {
+  const sizeObj = useScreenSize();
   const partners = [
     shidLogo,
     itnLogo,
@@ -52,35 +54,36 @@ function App() {
   const sectors = [
     {
       name: "Промышленность",
-      imgUrl: require("./assets/sectors-list/pexels-pixabay-326410 1.png"),
+      imgUrl: require("./assets/sectors-list/pexels-pixabay-326410.png"),
       description: "Чистые технологии, способствующие уменьшению выбросов из промышленных дымовых труб; улучшенная утилизация городских и сельскохозяйственных отходов, включая каптаж (улавливание) метана, выделяемого в местах утилизации отходов, в качестве варианта, альтернативного сжиганию (для использования в качестве биогаза)"
     },
     {
       name: "Транспорт",
-      imgUrl: require("./assets/sectors-list/pexels-cottonbro-4604564 1.png"),
+      imgUrl: require("./assets/sectors-list/pexels-cottonbro-4604564.png"),
       description: "Переход к чистым способам выработки энергии; уделение приоритетного внимания скоростному городскому транспорту, пешеходным и велосипедным сетям в городах, а также железнодорожным междугородным грузовым и пассажирским перевозкам; переход к использованию более чистых большегрузных дизельных транспортных средств и автомобилей с низким уровнем выбросов, а также более чистых видов топлива, включая топливо со сниженной концентрацией серы"
     },
     {
       name: "Городское планирование",
-      imgUrl: require("./assets/sectors-list/pexels-aleksejs-bergmanis-681335 1.png"),
+      imgUrl: require("./assets/sectors-list/pexels-aleksejs-bergmanis-681335.png"),
       description: "Улучшение энергетической эффективности зданий и обеспечение более зеленых и компактных и тем самым более энергоэффективных городов"
     },
     {
       name: "Энергия",
-      imgUrl: require("./assets/sectors-list/pexels-cottonbro-3992206 1.png"),
+      imgUrl: require("./assets/sectors-list/pexels-cottonbro-3992206.png"),
       description: "Обеспечение доступа к недорогостоящим источникам энергии в быту для приготовления пищи, отопления и освещения"
     },
     {
       name: "Энергетика",
-      imgUrl: require("./assets/sectors-list/pexels-flickr-157039 1.png"),
+      imgUrl: require("./assets/sectors-list/pexels-flickr-157039.png"),
       description: " Более широкое использование видов топлива с низким уровнем выбросов и возобновляемых источников энергии, не основанных на сжигании (таких как энергия солнца, ветра или гидроэнергия); комбинированная выработка тепла и энергии; и распределенная выработка энергии (например, энергетические минисистемы и размещаемые на крыше установки для выработки энергии из солнечной энергии)"
     },
     {
       name: "Утилизация отходов",
-      imgUrl: require("./assets/sectors-list/pexels-vladislav-vasnetsov-2682683 1.png"),
+      imgUrl: require("./assets/sectors-list/pexels-vladislav-vasnetsov-2682683.png"),
       description: "стратегии уменьшения отходов, сортировки отходов, рециклирования, повторного использования или переработки отходов; а также улучшенные методы биологической утилизации отходов, такие как анаэробная переработка отходов для производства биогаза, являются практически осуществимыми, недорогими альтернативными вариантами открытому сжиганию твердых отходов"
     },
   ];
+  console.log(sizeObj);
 
   return (
     <>
@@ -93,6 +96,7 @@ function App() {
                    subtitle="Есть много примеров успешной политики по уменьшению загрязнения воздуха в таких секторах, как транспорт, городское планирование, энергетика и промышленность:"
                    items={sectors}
       />
+
     </>
   );
 }
--- src/components/SectorsList/SectorsList.js
+++ src/components/SectorsList/SectorsList.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import './SectorsList.scss';
+import SectorItem from "./SectorItem";
 
 const SectorsList = ({title, subtitle, items}) => {
-	const itemsList = items.map((sector, i) => <div className="sector-list-item" style={{background: `utl(${sector.imgUrl}) center no-repeat / cover`}}>
-		<p className="sector-item-name">
-			{sector.name}
-		</p>
-		<p className="sector-item-tooltip">
-			{sector.description}
-		</p>
-	</div>)
+	const itemsList = items.map((sector, i) => <SectorItem sector={sector} number={i+1} key={i} />)
 	return (
 		<section className="sectors container">
 			<h2 className="sectors-title">{title}</h2>
 			<p className="sectors-subtitle">{subtitle}</p>
 			<div className="sectors-list">
-				<div className="sectors-list-item">
 					{itemsList}
-				</div>
 			</div>
 		</section>
 	);
