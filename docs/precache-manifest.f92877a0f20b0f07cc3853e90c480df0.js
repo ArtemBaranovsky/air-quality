70c55831c03ccd7b2f93ed3d95ea6d5adb799e0f
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2020-10-18 18:43:59 +0300

    Day 4 (Added static project build for github pages)

---
+++ docs/precache-manifest.f92877a0f20b0f07cc3853e90c480df0.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5652f48f82ac7b6aafacb04bb6dbf59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2914927bbb087efe1be1",
+    "url": "/static/css/2.24e2894b.chunk.css"
+  },
+  {
+    "revision": "c7d8b80d1534c8d864c1",
+    "url": "/static/css/main.fa79958b.chunk.css"
+  },
+  {
+    "revision": "2914927bbb087efe1be1",
+    "url": "/static/js/2.858b5e1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.858b5e1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7d8b80d1534c8d864c1",
+    "url": "/static/js/main.7dd4b091.chunk.js"
+  },
+  {
+    "revision": "52c56a57f4ed61038532",
+    "url": "/static/js/runtime-main.6d9367bc.js"
+  },
+  {
+    "revision": "0662d9de8af86fb66060a046fbafe1b5",
+    "url": "/static/media/GC_logo.0662d9de.svg"
+  },
+  {
+    "revision": "2f311a05a79fb86cee6148d658aa8336",
+    "url": "/static/media/ITN_logo.2f311a05.svg"
+  },
+  {
+    "revision": "f9ea7350b96a78bfb256fd7cc66b9de4",
+    "url": "/static/media/hero-bg.f9ea7350.jpg"
+  },
+  {
+    "revision": "ee8c3f3a066d2aaced7579dfc6fff667",
+    "url": "/static/media/pexels-aleksejs-bergmanis-681335.ee8c3f3a.png"
+  },
+  {
+    "revision": "c39d429db04802ec332a86a1f8c49c69",
+    "url": "/static/media/pexels-cottonbro-3992206.c39d429d.png"
+  },
+  {
+    "revision": "34b8eac4ddc3a52f5a60693b82c5d5a1",
+    "url": "/static/media/pexels-cottonbro-4604564.34b8eac4.png"
+  },
+  {
+    "revision": "c9026db6b944cd7b1ad28f85f47fb42d",
+    "url": "/static/media/pexels-flickr-157039.c9026db6.png"
+  },
+  {
+    "revision": "5c8f8ca9b726f67d8a69ab56dd28fa61",
+    "url": "/static/media/pexels-pixabay-326410.5c8f8ca9.png"
+  },
+  {
+    "revision": "bbfd8a576bd4909aba17c31f708dd2a3",
+    "url": "/static/media/pexels-vladislav-vasnetsov-2682683.bbfd8a57.png"
+  },
+  {
+    "revision": "341a487c2bb1cbed9179faa73d89b343",
+    "url": "/static/media/shid_logo_white.341a487c.svg"
+  },
+  {
+    "revision": "5574d6ecf65ccb0a6c22c237a02acaa5",
+    "url": "/static/media/slide-1.5574d6ec.jpg"
+  },
+  {
+    "revision": "261b9856b2ecca1ae2bb694f9eacab25",
+    "url": "/static/media/slide-2.261b9856.jpg"
+  },
+  {
+    "revision": "54f87ec43c16252e0930b4c8720f262f",
+    "url": "/static/media/slide-3.54f87ec4.jpg"
+  },
+  {
+    "revision": "f084f5edb225f3a1fc8149a56d2fca6f",
+    "url": "/static/media/slide-4.f084f5ed.jpg"
+  },
+  {
+    "revision": "d3a6a7b0391706f583d0210c3165df2b",
+    "url": "/static/media/slide-5.d3a6a7b0.jpg"
+  }
+]);
