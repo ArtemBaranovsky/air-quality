278f61fb3bd0d580e510f730bd3cb578582c0186
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2020-10-20 09:21:54 +0300

    Day 4 (Added static project build for github pages)

---
+++ docs/precache-manifest.85e21f0ed617d39c315762a84e28d19f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "636709f34e2e6f89e57c1df98c45195e",
+    "url": "/air-quality/index.html"
+  },
+  {
+    "revision": "1c257a0237a758b850ce",
+    "url": "/air-quality/static/css/2.24e2894b.chunk.css"
+  },
+  {
+    "revision": "ac84d2eb9e15bdf7d0cb",
+    "url": "/air-quality/static/css/main.c3e992ee.chunk.css"
+  },
+  {
+    "revision": "1c257a0237a758b850ce",
+    "url": "/air-quality/static/js/2.074e01f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/air-quality/static/js/2.074e01f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac84d2eb9e15bdf7d0cb",
+    "url": "/air-quality/static/js/main.e6650901.chunk.js"
+  },
+  {
+    "revision": "be8b00b45510cbbb35d0",
+    "url": "/air-quality/static/js/runtime-main.2c0b8cd0.js"
+  },
+  {
+    "revision": "e60be19e8cbfbd550ac9a3f905ad359a",
+    "url": "/air-quality/static/media/GC_logo.e60be19e.svg"
+  },
+  {
+    "revision": "899cd2784349ab5b5a24f5dcee2ffca3",
+    "url": "/air-quality/static/media/ITN_logo.899cd278.svg"
+  },
+  {
+    "revision": "f9ea7350b96a78bfb256fd7cc66b9de4",
+    "url": "/air-quality/static/media/hero-bg.f9ea7350.jpg"
+  },
+  {
+    "revision": "ee8c3f3a066d2aaced7579dfc6fff667",
+    "url": "/air-quality/static/media/pexels-aleksejs-bergmanis-681335.ee8c3f3a.png"
+  },
+  {
+    "revision": "c39d429db04802ec332a86a1f8c49c69",
+    "url": "/air-quality/static/media/pexels-cottonbro-3992206.c39d429d.png"
+  },
+  {
+    "revision": "34b8eac4ddc3a52f5a60693b82c5d5a1",
+    "url": "/air-quality/static/media/pexels-cottonbro-4604564.34b8eac4.png"
+  },
+  {
+    "revision": "c9026db6b944cd7b1ad28f85f47fb42d",
+    "url": "/air-quality/static/media/pexels-flickr-157039.c9026db6.png"
+  },
+  {
+    "revision": "5c8f8ca9b726f67d8a69ab56dd28fa61",
+    "url": "/air-quality/static/media/pexels-pixabay-326410.5c8f8ca9.png"
+  },
+  {
+    "revision": "bbfd8a576bd4909aba17c31f708dd2a3",
+    "url": "/air-quality/static/media/pexels-vladislav-vasnetsov-2682683.bbfd8a57.png"
+  },
+  {
+    "revision": "8dd4ef7149a55ade70696aa0e9e2faa3",
+    "url": "/air-quality/static/media/shid_logo_white.8dd4ef71.svg"
+  },
+  {
+    "revision": "5574d6ecf65ccb0a6c22c237a02acaa5",
+    "url": "/air-quality/static/media/slide-1.5574d6ec.jpg"
+  },
+  {
+    "revision": "261b9856b2ecca1ae2bb694f9eacab25",
+    "url": "/air-quality/static/media/slide-2.261b9856.jpg"
+  },
+  {
+    "revision": "54f87ec43c16252e0930b4c8720f262f",
+    "url": "/air-quality/static/media/slide-3.54f87ec4.jpg"
+  },
+  {
+    "revision": "f084f5edb225f3a1fc8149a56d2fca6f",
+    "url": "/air-quality/static/media/slide-4.f084f5ed.jpg"
+  },
+  {
+    "revision": "d3a6a7b0391706f583d0210c3165df2b",
+    "url": "/air-quality/static/media/slide-5.d3a6a7b0.jpg"
+  }
+]);
